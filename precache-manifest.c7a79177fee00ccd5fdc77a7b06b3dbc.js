c93500a74917612ccc1d7a9088d5e7efc0d49630
Wei, EnTin | Tim | TWR <entin.wei@rakuten.com>
2021-03-18 22:05:38 +0800

    Updates

---
+++ precache-manifest.c7a79177fee00ccd5fdc77a7b06b3dbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38a68461d98f1bafcb9744732a45fc9",
+    "url": "/song-search-xstate-local/index.html"
+  },
+  {
+    "revision": "8d44947772c91db65ff4",
+    "url": "/song-search-xstate-local/static/css/main.3cb53230.chunk.css"
+  },
+  {
+    "revision": "e19581d1b25d9fe0af94",
+    "url": "/song-search-xstate-local/static/js/2.b65ce102.chunk.js"
+  },
+  {
+    "revision": "c24bd15efa6a02fc028f6ddc87879af3",
+    "url": "/song-search-xstate-local/static/js/2.b65ce102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d44947772c91db65ff4",
+    "url": "/song-search-xstate-local/static/js/main.7dd54f85.chunk.js"
+  },
+  {
+    "revision": "ac360cb5fd05a677585a",
+    "url": "/song-search-xstate-local/static/js/runtime-main.1f90d648.js"
+  },
+  {
+    "revision": "4f744e8239d2552ad4ddf0b89b75a046",
+    "url": "/song-search-xstate-local/static/media/cross.4f744e82.svg"
+  },
+  {
+    "revision": "0f51771c9da2d2678d0c8c22a4867ab0",
+    "url": "/song-search-xstate-local/static/media/loading.0f51771c.svg"
+  },
+  {
+    "revision": "fa72ba04bd8bb65f81e0bef1804b0ace",
+    "url": "/song-search-xstate-local/static/media/search.fa72ba04.svg"
+  }
+]);
